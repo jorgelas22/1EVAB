3f08a47f98cd6db5c42727458014783b8c5dc6a0
1dam <jorgeberlanga36@gmail.com>
2025-11-24 10:03:37 +0100

    Arreglo de variable

--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class main {
 
+    public static final String AGENDA = "\n=== AGENDA ===";
+    public static final String LISTAR = "1. Listar";
+    public static final String BUSCAR_POR_DÍA = "2. Buscar por día";
+    public static final String BUSCAR_POR_NOMBRE = "3. Buscar por nombre";
+    public static final String AÑADIR = "4. Añadir";
+    public static final String ELIMINAR = "5. Eliminar";
+    public static final String SALIR = "6. Salir";
+
     public static void main(String[] args) {
 
         cumpleRepo repo = new cumpleRepo();
@@ -11,13 +19,13 @@ public class main {
         boolean salir = false;
 
         while (!salir) {
-            System.out.println("\n=== AGENDA ===");
-            System.out.println("1. Listar");
-            System.out.println("2. Buscar por día");
-            System.out.println("3. Buscar por nombre");
-            System.out.println("4. Añadir");
-            System.out.println("5. Eliminar");
-            System.out.println("6. Salir");
+            System.out.println(AGENDA);
+            System.out.println(LISTAR);
+            System.out.println(BUSCAR_POR_DÍA);
+            System.out.println(BUSCAR_POR_NOMBRE);
+            System.out.println(AÑADIR);
+            System.out.println(ELIMINAR);
+            System.out.println(SALIR);
 
             String opcion = sc.nextLine();
 
